744ce26afc8871da2167aa0e18f621fceb47f1df
Karl Hardenstine <khardenstine@gmail.com>
2014-02-02 13:26:03 -0500

    Moved Animation

--- src/main/scala/Entity.scala
+++ src/main/scala/Entity.scala
@@ -16,39 +16,3 @@ abstract class Entity(val position: Point) extends IEntity
 		this(new Point(x, y))
 	}
 }
-
-trait Animation
-{
-	def animate(time: Long): Unit
-}
-
-trait StepAnimation extends Animation
-{
-	protected val animationStepLength: Long
-	private var lastStepTime: Option[Long] = None
-
-	def animate(time: Long): Unit = {
-		var diff = time - lastStepTime.getOrElse(time - animationStepLength)
-		if (diff >= animationStepLength)
-		{
-			lastStepTime = Some(time)
-
-			while (diff >= animationStepLength)
-			{
-				animationStep()
-				diff -= animationStepLength
-			}
-		}
-	}
-
-	protected def animationStep()
-}
-
-trait MultiAnimated
-{
-	protected val animations: Seq[Animation]
-
-	def animate(time: Long): Unit = {
-		animations.foreach(_.animate(time))
-	}
-}
